5cd92a7a7f0d67ca108bdcb25230d8bfb40165d0
MMTabaka <miloszmtabaka@gmail.com>
2021-08-11 19:24:56 +0100

    Code refactoring

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -1,5 +1,5 @@
-def state(player, grid):
-    winning = player + ' wins'
+# Returns True if a given player wins in a given grid
+def is_winning(player, grid):
     if grid[4] == player:
         if grid[0] == player and grid[8] == player:
             return True
@@ -22,6 +22,7 @@ def state(player, grid):
     return False
 
 
+# Prints a grid based on characters in a string input
 def print_grid(grid):
     print('---------')
     print('| ' + grid[0] + ' ' + grid[1] + ' ' + grid[2] + ' |')
@@ -30,61 +31,76 @@ def print_grid(grid):
     print('---------')
 
 
+# Checks if user's input is allowed
+def are_coords_valid(x, y, grid):
+    if isinstance(x, int) and isinstance(y, int):
+        if 0 < x < 4 and 0 < y < 4:
+            cell = (x - 1) * 3 + y - 1
+            if grid[cell] == '_':
+                return True
+            else:
+                print('This cell is occupied! Choose another one!')
+                return False
+        else:
+            print('Coordinates should be from 1 to 3!')
+            return False
+    else:
+        print('You should enter numbers!')
+        return False
+
+
+# Generates new grid by placing given player in a square indicated by entered coordinates
 def enter_coords(player, grid):
-    incorrect_input = True
-    while incorrect_input:
+    coords = input('Enter the coordinates: ').split()
+    x = int(coords[0])
+    y = int(coords[1])
+
+    while not are_coords_valid(x, y, grid):
         coords = input('Enter the coordinates: ').split()
         x = int(coords[0])
         y = int(coords[1])
+    else:
+        cell = (x - 1) * 3 + y - 1
+        grid = grid[0:cell] + player + grid[cell + 1:]
 
-        if isinstance(x, int) and isinstance(y, int):
-            if 0 < x < 4 and 0 < y < 4:
-                cell = (x - 1) * 3 + y - 1
-                if grid[cell] == '_':
-                    grid = grid[0:cell] + player + grid[cell+1:]
-                    incorrect_input = False
-                else:
-                    print('This cell is occupied! Choose another one!')
-            else:
-                print('Coordinates should be from 1 to 3!')
-        else:
-            print('You should enter numbers!')
     print_grid(grid)
     return grid
 
 
+# Returns True if given input represents any of the ending game states
 def game_over(grid):
-    if abs(grid.count('X') - grid.count('O')) > 1 or state('X', grid) and state('O', grid):
+    if abs(grid.count('X') - grid.count('O')) > 1 or is_winning('X', grid) and is_winning('O', grid):
         print('Impossible')
         return True
     else:
-        if state('X', grid):
+        if is_winning('X', grid):
             return True
-        elif state('O', grid):
+        elif is_winning('O', grid):
             return True
         elif '_' not in grid:
             return True
 
 
-def print_result(grid):
-    if state('X', grid):
+def print_winner(grid):
+    if is_winning('X', grid):
         print('X wins')
-    elif state('O', grid):
+    elif is_winning('O', grid):
         print('O wins')
     elif '_' not in grid:
         print('Draw')
 
 
-grid_play = '_________'
-print_grid(grid_play)
-player = 'X'
+# Gameplay - starts with empty grid and X as a player
+current_grid = '_________'
+print_grid(current_grid)
+current_player = 'X'
 
-while not game_over(grid_play):
-    grid_play = enter_coords(player, grid_play)
-    game_over(grid_play)
-    if player == 'X':
-        player = 'O'
+while not game_over(current_grid):
+    current_grid = enter_coords(current_player, current_grid)
+    game_over(current_grid)
+    if current_player == 'X':
+        current_player = 'O'
     else:
-        player = 'X'
+        current_player = 'X'
 else:
-    print_result(grid_play)
+    print_winner(current_grid)
